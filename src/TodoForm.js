d0ea3d9db9615108800da48fd66747a92dccc325
wdwgrg <93629595+wdwgrg@users.noreply.github.com1~>
2022-02-22 02:28:01 +0000

    TodoForm Input test ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 const TodosFrom = () => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    },[]);
     return (
         <form>
-            <input placeholder="할 일을 입력해 주세요~"/>
+            <input placeholder="할 일을 입력해 주세요~" value={value} onChange={onChange}/>
             <button type = "submit">등록</button>
         </form>
     );
