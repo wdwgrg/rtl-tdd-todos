7a89ac2f259f1276a11948e26b389f4d4604c728
wdwgrg <93629595+wdwgrg@users.noreply.github.com1~>
2022-02-22 06:11:21 +0000

    TodoItem refactoring ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const TodoItem = ({todo, onToggle, onRemove}) => {
     const {id, text, done} = todo;
+    const toggle = useCallback(()=> onToggle(id),[id,onToggle]);
+    const remove = useCallback(()=>onRemove(id),[id,onRemove]);
     return (
     <li>
         <span
             style={{textDecoration: done ? 'line-through' : 'none'}}
-        onClick={()=> onToggle(id)}>{text}</span>
-        <button onClick={()=>onRemove(id)}>삭제</button>
+        onClick={toggle}
+        >
+            {text}
+        </span>
+        <button onClick={remove}>삭제</button>
 
     </li>
     );
